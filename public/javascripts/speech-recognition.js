4edec27d977d0780a3f86427166b53a10c7f5834
Mdkerner <Mdkerner@gmail.com>
2016-06-08 11:36:22 -0700

    Adding voice recognition

---
+++ public/javascripts/speech-recognition.js
@@ -0,0 +1,29 @@
+var recognition = new webkitSpeechRecognition();
+recognition.continuous = true;
+recognition.interimResults = true;
+
+recognition.onStart = function(){
+    if (recognizing) {
+        recognition.stop();
+        return;
+    }
+    recognizing = true;
+    recognition.lang = "en-US";
+}
+
+recognition.onresult = function(event) {
+    var final_transcript = "";
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+            final_transcript += event.results[i][0].transcript;
+        }
+        else {
+        	$("#speech-input").text += event.results[i][0].transcript;
+      }
+    }
+    alert("final_transcript");
+};
+
+function startSpeechRec(){
+    recognition.start();
+}
