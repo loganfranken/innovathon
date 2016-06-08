24422281900bf8b408bd3c9c6e84f38336d4b061
Logan Franken <loganfranken@gmail.com>
2016-06-08 12:36:20 -0700

    Integrated location data

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,28 @@
-$(function() {
+require(["esri/geometry/webMercatorUtils"], function(webMercatorUtils) {
 
-  $('#question-form').on('submit', function(event) {
+  $(function() {
 
-    event.preventDefault();
+    $('#question-form').on('submit', function(event) {
 
-    var question = $('#question').val();
+      event.preventDefault();
 
-    $.get('/answers?question=' + question, function(data) {
-      $('#response').html(data);
+      var question = $('#question').val();
+
+      $.get('/answers?question=' + question, function(data) {
+
+        var jsonData = JSON.stringify(data);
+        if(jsonData.match(/fid/ig))
+        {
+          var coordinates = /-?[0-9]+.[0-9]+,-?[0-9]+.[0-9]+/.exec(jsonData)[0].split(',');
+          var latLong = webMercatorUtils.xyToLngLat(coordinates[0], coordinates[1]);
+          $('#response').html('<iframe width="500" height="500" src="https://www.google.com/maps/embed/v1/view?key=AIzaSyBjpNq9goKydvdeU63xYsbUAVETtwQJNs4&center=' + latLong[1] + ',' + latLong[0] + '&zoom=19">');
+        }
+        else {
+          $('#response').html(data);
+        }
+
+      });
+
     });
 
   });
